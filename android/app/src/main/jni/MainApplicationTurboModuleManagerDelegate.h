e4267a7fc2e082e7ad3e7f3b1f5c4f415e60bc94
bharath.doijode <bharath.doijode@olamnet.com>
2024-07-05 20:34:31 +0530

    rename

--- android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
+++ android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
@@ -14,7 +14,7 @@ class MainApplicationTurboModuleManagerDelegate
  public:
   // Adapt it to the package you used for your Java class.
   static constexpr auto kJavaDescriptor =
-      "Lcom/news/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
+      "Lcom/mindsprint/farmerLead/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
 
   static jni::local_ref<jhybriddata> initHybrid(jni::alias_ref<jhybridobject>);
 
--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/news/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/mindsprint/farmerLead/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
